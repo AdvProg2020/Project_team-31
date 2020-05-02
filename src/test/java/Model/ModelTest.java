077fee69a74304fcdfdad02935e8b57cce020e63
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2020-05-02 11:50:50 +0430

    fixing bugs

--- src/test/java/Model/ModelTest.java
+++ src/test/java/Model/ModelTest.java
@@ -8,14 +8,5 @@ import java.util.Date;
 
 public class ModelTest {
 
-    @Test
-    public void buyingLogTest(){
-        ArrayList<Product> buying = new ArrayList<Product>();
-        ArrayList<String> specialProperties = new ArrayList<String>();
-        specialProperties.add("length");
-        specialProperties.add("price");
-        buying.add(new Product("1234" , "pants" , "poma",  2000. , new Category("Dress" ,specialProperties ) , true , ProductStatus.creating , "this is perfect" , new Seller("mmd" , "cheragi" , "kaka" , "mmdkaka2000@gmail.com" , 23456789 , "salam man be to yar ghadimi" , "poma")));
-        BuyingLog buyingLog = new BuyingLog( "1234_salam", new Date(),1000.0 , 200.0 , "Pink" , DeliveryStatus.sending , new Customer("hamed" , "Rashidpour" ,"Mr.Pink" , "hamedrashid@gmail.com" , 12345467 , "pinkis18" ) , buying  );
-        System.out.println(buyingLog);
-    }
+
 }
