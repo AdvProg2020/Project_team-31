27f2cb7151f949e2399b31d44fd49a3dd7b81bad
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2020-05-15 12:01:27 +0430

    edit payment view

--- src/main/java/View/CompletionShop.java
+++ src/main/java/View/CompletionShop.java
@@ -30,29 +30,33 @@ public class CompletionShop extends Menu {
     private BuyingLog getInformation() throws Exception {
         String[] data = new String[2];
         System.out.println("please enter your address : ");
-        data[0] = scanner.nextLine();
-        if(data[0].equalsIgnoreCase("back"))
-            throw new Exception("cancel");
+        data[0] = scanner.nextLine().trim();
+        if (data[0].equalsIgnoreCase("back"))
+            throw new Exception("canceled!");
         System.out.println("please enter your phone number : ");
-        data[1] = scanner.nextLine();
-        if(data[0].equalsIgnoreCase("back"))
-            throw new Exception("cancel");
+        data[1] = scanner.nextLine().trim();
+        if (data[1].equalsIgnoreCase("back"))
+            throw new Exception("canceled!");
         return customerController.createBuyingLog(user, data);
     }
 
-    private void discountCode(BuyingLog buyingLog) {
+    private void discountCode(BuyingLog buyingLog) throws Exception {
         System.out.println("please enter your discount code : ");
-        String discountCode = scanner.nextLine();
+        String discountCode = scanner.nextLine().trim();
+        if (discountCode.equalsIgnoreCase("back"))
+            throw new Exception("canceled!");
         try {
             customerController.putDiscount(user, buyingLog, discountCode);
         } catch (Exception e) {
             System.out.println(e.getMessage());
-
         }
     }
 
-    private void payment(BuyingLog buyingLog) {
-
-
+    private void payment(BuyingLog buyingLog) throws Exception {
+        System.out.println("are you sure about your purchase? (yes/no)");
+        String command = scanner.nextLine().trim();
+        if (command.equalsIgnoreCase("no") || command.equalsIgnoreCase("back"))
+            throw new Exception("canceled!");
+        customerController.payMoney(user, buyingLog);
     }
 }
