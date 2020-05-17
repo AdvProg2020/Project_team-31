c3a28030a250ea8cad9d82c97e35a202b77ee355
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2020-05-17 18:38:27 +0430

    edit login view

--- src/main/java/View/LoginMenu.java
+++ src/main/java/View/LoginMenu.java
@@ -92,7 +92,7 @@ public class LoginMenu extends Menu {
         System.out.println("please enter your password:");
         String password = scanner.nextLine();
         try {
-            loginController.login(username, password,);
+            loginController.login(username, password,card);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
