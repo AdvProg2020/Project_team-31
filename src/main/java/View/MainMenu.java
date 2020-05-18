3b2e7f425bc1d74ea7b90877c005409a284d1f13
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2020-05-18 18:49:28 +0430

    edit personalInfo view

--- src/main/java/View/MainMenu.java
+++ src/main/java/View/MainMenu.java
@@ -91,6 +91,7 @@ public class MainMenu extends Menu {
 
     private void viewPersonalInformation() throws Exception {
         mustLoginAccess();
+        System.out.println("/////////////////////////////////////////");
         String[] information = loginController.showPersonalInformation(user);
         System.out.println("first name : " + information[0] +
                 "\nlast name : " + information[1] +
@@ -102,6 +103,7 @@ public class MainMenu extends Menu {
         if (user instanceof Seller) {
             viewCompanyInformation();
         }
+        System.out.println("/////////////////////////////////////////");
         String command;
         while (!(command = scanner.nextLine().trim()).equalsIgnoreCase("back")) {
             Matcher matcher = getMatcher("^(?i)edit\\s+(.+)$", command);
