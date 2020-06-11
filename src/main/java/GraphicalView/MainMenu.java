9d66ab1dcbf01fc58dcd06a76230a7ec2926191c
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2020-06-11 19:05:47 +0430

    add MainMenu

---
+++ src/main/java/GraphicalView/MainMenu.java
@@ -0,0 +1,14 @@
+package GraphicalView;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainMenu extends Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // if there is not any manager make one...
+    }
+}
