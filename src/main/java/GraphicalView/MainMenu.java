70d80088bd68e79ab0d16123938bd9c3736b3956
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2020-06-11 19:49:37 +0430

    add some alerts to MainMenu

--- src/main/java/GraphicalView/MainMenu.java
+++ src/main/java/GraphicalView/MainMenu.java
@@ -1,14 +1,43 @@
 package GraphicalView;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainMenu implements Initializable {
+    public Button mainMenuLogout;
+    public Button userArea;
+    DataBase dataBase = DataBase.getInstance();
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // if there is not any manager make one...
+        /////////////////////////////////////////////////////////
+        userAreaAlert();
+        logoutAlert();
+
     }
+
+    private void userAreaAlert() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        EventHandler<ActionEvent> event = (e) -> alert.show();
+        if (dataBase.user == null)
+            userArea.setOnAction(event);
+        alert.setContentText("You have to login");
+    }
+
+    private void logoutAlert() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        EventHandler<ActionEvent> event = (e) -> alert.show();
+        if (dataBase.user == null)
+            mainMenuLogout.setOnAction(event);
+        alert.setContentText("You have to login");
+    }
+
 }
