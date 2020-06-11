9d3667bb1c7d52d70c6a2c8f25d3f07bf557d2d3
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2020-06-12 01:08:57 +0430

    edit view BuyingHistory

--- src/main/java/GraphicalView/BuyingHistory.java
+++ src/main/java/GraphicalView/BuyingHistory.java
@@ -1,11 +1,39 @@
 package GraphicalView;
 
+import Controller.CustomerController;
+import Model.Customer;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
-public class BuyingHistory {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class BuyingHistory implements Initializable {
+    public Label buyingHistory;
     Runner runner = Runner.getInstance();
+    DataBase dataBase = DataBase.getInstance();
+    CustomerController customerController = CustomerController.getInstance();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loadBuyingHistory();
+
+    }
+
+    private void loadBuyingHistory() {
+        ArrayList<String> logs = customerController.showAllOrders(dataBase.user);
+        StringBuilder data = new StringBuilder();
+        for (String log : logs) {
+            data.append(log).append("\n");
+        }
+        buyingHistory.setText(data.toString());
+    }
 
     public void back(ActionEvent actionEvent) {
         runner.back();
     }
+
+
 }
