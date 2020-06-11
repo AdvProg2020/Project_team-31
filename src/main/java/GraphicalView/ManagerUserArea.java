e9aa8d9b404f4533018d448829b2195f973dbad2
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2020-06-11 22:57:08 +0430

    implement SellerUserArea

--- src/main/java/GraphicalView/ManagerUserArea.java
+++ src/main/java/GraphicalView/ManagerUserArea.java
@@ -1,4 +1,11 @@
 package GraphicalView;
 
+import javafx.scene.input.MouseEvent;
+
 public class ManagerUserArea {
+    Runner runner = Runner.getInstance();
+
+    public void back(MouseEvent mouseEvent) {
+        runner.back();
+    }
 }
