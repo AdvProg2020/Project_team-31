3567eea32a9a939f2b5c3730a7982d4b3f9e94f0
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2020-06-11 19:03:39 +0430

    add DataBase.java

--- src/main/java/GraphicalView/DataBase.java
+++ src/main/java/GraphicalView/DataBase.java
@@ -1,4 +1,23 @@
 package GraphicalView;
 
+import Model.User;
+
+import java.net.URL;
+import java.util.Stack;
+
 public class DataBase {
+    public static DataBase dataBase = null;
+
+    public static DataBase getInstance() {
+        if (dataBase == null)
+            dataBase = new DataBase();
+        return dataBase;
+    }
+
+    private DataBase() {
+    }
+
+    ///////////////////////////////////////////////////////////
+    Stack<URL> pages = new Stack();
+    User user = null;
 }
