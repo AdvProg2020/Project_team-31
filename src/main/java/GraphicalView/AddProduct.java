cf479220f0258d156d7e38f775a1697a56638c4e
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2020-06-12 19:08:26 +0430

    implement AddProduct

--- src/main/java/GraphicalView/AddProduct.java
+++ src/main/java/GraphicalView/AddProduct.java
@@ -1,4 +1,14 @@
 package GraphicalView;
 
+import javafx.event.ActionEvent;
+
 public class AddProduct {
+    Runner runner=Runner.getInstance();
+    public void submit(ActionEvent actionEvent) {
+
+    }
+
+    public void back(ActionEvent actionEvent) {
+        runner.back();
+    }
 }
