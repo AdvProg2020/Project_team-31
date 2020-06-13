ed4e2789b689f95178d02802c702fdf2d56b24e6
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2020-06-13 16:35:46 +0430

    add loginAlert to SellerHistory

--- src/main/java/GraphicalView/SellerHistory.java
+++ src/main/java/GraphicalView/SellerHistory.java
@@ -1,11 +1,51 @@
 package GraphicalView;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 
-public class SellerHistory {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SellerHistory implements Initializable {
+    public Button login;
+    public Button logout;
+
     Runner runner = Runner.getInstance();
+    DataBase dataBase = DataBase.getInstance();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loginAlert();
+        logoutAlert();
+    }
 
     public void back(ActionEvent actionEvent) {
         runner.back();
+    }
+
+    private void logoutAlert() {
+        Alert message = new Alert(Alert.AlertType.INFORMATION);
+        EventHandler<ActionEvent> event = (e) -> {
+            message.setContentText("you logged out successfully");
+            message.show();
+            dataBase.logout();
+        };
+        logout.setOnAction(event);
+    }
+
+    public void loginAlert() {
+        Alert error = new Alert(Alert.AlertType.ERROR);
+        EventHandler<ActionEvent> event = (e) -> {
+            if (dataBase.user != null) {
+                error.setContentText("You have logged in!");
+                error.show();
+            } else {
+                runner.changeScene("LoginMenu.fxml");
+            }
+        };
+        login.setOnAction(event);
     }
 }
