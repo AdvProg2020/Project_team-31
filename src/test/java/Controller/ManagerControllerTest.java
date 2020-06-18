4bc7c543a8061331f3acf665c6d785fbfe977742
hamedrashidpour <45601809+hamedrashidpour@users.noreply.github.com>
2020-06-18 23:04:58 +0430

    create unit test in master branch

--- src/test/java/Controller/ManagerControllerTest.java
+++ src/test/java/Controller/ManagerControllerTest.java
@@ -1,7 +1,88 @@
 package Controller;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ManagerControllerTest {
 
+    @Test
+    public void getInstance() {
+    }
+
+    @Test
+    public void showUsers() {
+    }
+
+    @Test
+    public void deleteUser() {
+    }
+
+    @Test
+    public void createDiscountCode() {
+    }
+
+    @Test
+    public void showAllDiscountCodes() {
+    }
+
+    @Test
+    public void showDiscount() {
+    }
+
+    @Test
+    public void getDiscountById() {
+    }
+
+    @Test
+    public void editDiscountCode() {
+    }
+
+    @Test
+    public void removeDiscountCode() {
+    }
+
+    @Test
+    public void showAllRequests() {
+    }
+
+    @Test
+    public void showRequestDetails() {
+    }
+
+    @Test
+    public void acceptRequest() {
+    }
+
+    @Test
+    public void declineRequest() {
+    }
+
+    @Test
+    public void showAllCategories() {
+    }
+
+    @Test
+    public void addCategory() {
+    }
+
+    @Test
+    public void removeCategory() {
+    }
+
+    @Test
+    public void editCategory() {
+    }
+
+    @Test
+    public void changeFeatureOfCategory() {
+    }
+
+    @Test
+    public void getCategoryByName() {
+    }
+
+    @Test
+    public void getCategoryFeaturesOfAProduct() {
+    }
 }
