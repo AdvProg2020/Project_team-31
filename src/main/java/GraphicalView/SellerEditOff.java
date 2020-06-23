9256a1de77efd5641c3f5dc3c9111a4d6dd9c0c1
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2020-06-23 17:33:09 +0430

    add SellerEditOff

---
+++ src/main/java/GraphicalView/SellerEditOff.java
@@ -0,0 +1,16 @@
+package GraphicalView;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SellerEditOff implements Initializable {
+    Runner runner = Runner.getInstance();
+    DataBase dataBase = DataBase.getInstance();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
