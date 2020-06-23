6ce9671d76ef5e5a429d8c6591e0452ba06f3410
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2020-06-23 22:30:28 +0430

    implement EditProduct

--- src/main/java/GraphicalView/EditProduct.java
+++ src/main/java/GraphicalView/EditProduct.java
@@ -1,14 +1,21 @@
 package GraphicalView;
 
+import Controller.ManagerController;
+import Model.Product;
+import Model.Seller;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.Node;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
+import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class EditProduct implements Initializable {
@@ -19,15 +26,26 @@ public class EditProduct implements Initializable {
     public TextField number;
     public VBox choiceBoxContainer;
     public Button categoryFeaturesButton;
+    StringProperty categoryName = new SimpleStringProperty("");
     Runner runner = Runner.getInstance();
     DataBase dataBase = DataBase.getInstance();
+    Product product = ProductsMenu.product;
+    ChoiceBox<String> choiceBox;
+    File photo;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        dropDownListSetUp();
         initValues();
     }
 
     private void initValues() {
+        productName.setText(product.getName());
+        companyName.setText(product.getCompany());
+        price.setText(product.getSellersOfThisProduct().get((Seller) dataBase.user);
+        description.setText(product.getInformation());
+        number.setText(String.valueOf(product.getAvailable()));
+        //... init choice box
 
     }
 
@@ -41,5 +59,14 @@ public class EditProduct implements Initializable {
 
     public void selectFile(MouseEvent mouseEvent) {
 
+    }
+
+    private void dropDownListSetUp() {
+        choiceBox = new ChoiceBox<>();
+        ArrayList<String> categories = ManagerController.getInstance().showAllCategories();
+        choiceBox.getItems().addAll(categories);
+        choiceBoxContainer.getChildren().add(choiceBox);
+        categoryName.bind(choiceBox.valueProperty());
+        choiceBox.setValue(product.getCategory().getName());
     }
 }
