072b49a682385a71d0f1bc8c3a3b3fdf9f412626
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2020-06-23 21:08:59 +0430

    add EditProduct

--- src/main/java/GraphicalView/EditProduct.java
+++ src/main/java/GraphicalView/EditProduct.java
@@ -1,4 +1,45 @@
 package GraphicalView;
 
-public class EditProduct {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EditProduct implements Initializable {
+    public TextField productName;
+    public TextField companyName;
+    public TextField price;
+    public TextArea description;
+    public TextField number;
+    public VBox choiceBoxContainer;
+    public Button categoryFeaturesButton;
+    Runner runner = Runner.getInstance();
+    DataBase dataBase = DataBase.getInstance();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initValues();
+    }
+
+    private void initValues() {
+
+    }
+
+    public void back(ActionEvent actionEvent) {
+        runner.back();
+    }
+
+    public void submit(ActionEvent actionEvent) {
+
+    }
+
+    public void selectFile(MouseEvent mouseEvent) {
+
+    }
 }
