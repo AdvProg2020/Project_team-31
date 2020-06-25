353353e8236143dfce9600393749ef68b9293058
hamedrashidpour <45601809+hamedrashidpour@users.noreply.github.com>
2020-06-25 15:53:12 +0430

    create SellerControllerTest class

---
+++ src/test/java/Controller/SellerControllerTest.java
@@ -0,0 +1,84 @@
+package Controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SellerControllerTest {
+
+    @Test
+    public void getInstance() {
+    }
+
+    @Test
+    public void showCompanyInformation() {
+    }
+
+    @Test
+    public void addSellerToProduct() {
+    }
+
+    @Test
+    public void showSalesHistory() {
+    }
+
+    @Test
+    public void showBuyersOfThisProduct() {
+    }
+
+    @Test
+    public void removeProductFromUser() {
+    }
+
+    @Test
+    public void showProductsOfThisSeller() {
+    }
+
+    @Test
+    public void getCategoryFeatures() {
+    }
+
+    @Test
+    public void addProduct() {
+    }
+
+    @Test
+    public void editProduct() {
+    }
+
+    @Test
+    public void removeProduct() {
+    }
+
+    @Test
+    public void showAllOffs() {
+    }
+
+    @Test
+    public void showOff() {
+    }
+
+    @Test
+    public void addOff() {
+    }
+
+    @Test
+    public void checkTimeOfOffs() {
+    }
+
+    @Test
+    public void editOff() {
+    }
+
+    @Test
+    public void getOffById() {
+    }
+
+    @Test
+    public void showBalanceOfSeller() {
+    }
+
+    @Test
+    public void getOffProducts() {
+    }
+}
