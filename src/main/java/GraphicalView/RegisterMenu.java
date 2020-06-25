78d023876b76a1135f2e575e95a2402cf616aace
ASUS <mohammad.cheraghi80@gmail.com>
2020-06-25 22:49:49 +0430

    fix bug

--- src/main/java/GraphicalView/RegisterMenu.java
+++ src/main/java/GraphicalView/RegisterMenu.java
@@ -74,9 +74,7 @@ public class RegisterMenu implements Initializable {
                         Runner.getInstance().back();
                         inform = new Alert(Alert.AlertType.INFORMATION, "You have added manager successfully!", ButtonType.OK);
                     } else {
-                        DataBase.getInstance().pages.pop();
-                        DataBase.getInstance().pages.pop();
-                        Runner.getInstance().changeScene("LoginMenu.fxml");
+                        Runner.getInstance().back();
                         inform = new Alert(Alert.AlertType.INFORMATION, "You have registered successfully!", ButtonType.OK);
                     }
                     inform.show();
