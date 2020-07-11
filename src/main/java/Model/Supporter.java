0b4360eaaf17d18f30abfab98d9380da2c6b5318
mohammadali kakavand <62176623+mohammadalikakavand@users.noreply.github.com>
2020-07-11 11:19:47 +0430

    add Supporter

---
+++ src/main/java/Model/Supporter.java
@@ -0,0 +1,7 @@
+package Model;
+
+public class Supporter extends User {
+    public Supporter(String name, String lastName, String username, String emailAddress, String phoneNumber, String password) {
+        super(name, lastName, username, emailAddress, phoneNumber, password);
+    }
+}
