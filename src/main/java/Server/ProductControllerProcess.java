6f58c56f335e5bf5d08e928881f9f213020b03c9
ASUS <mohammad.cheraghi80@gmail.com>
2020-07-16 15:58:57 +0430

    complete show products

--- src/main/java/Server/ProductControllerProcess.java
+++ src/main/java/Server/ProductControllerProcess.java
@@ -2,6 +2,7 @@ package Server;
 
 import Controller.ProductController;
 import Model.Category;
+import Model.Product;
 import Model.User;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -41,7 +42,33 @@ public class ProductControllerProcess {
             filters.add(fil);
         }
         output.add("filters", filters);
+        output.add("products", createArrayOfAllProducts());
         return output;
+    }
+
+    private JsonArray createArrayOfAllProducts() {
+        JsonArray allProducts = new JsonArray();
+        for (Product product : Product.getAllProducts()) {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("id", product.getProductId());
+            jsonObject.addProperty("name", product.getName());
+            jsonObject.addProperty("rate", product.getRate());
+            jsonObject.addProperty("minimumPrice", product.getMinimumPrice());
+            jsonObject.addProperty("categoryName", product.getCategory().getName());
+            jsonObject.addProperty("available", product.getAvailable());
+            jsonObject.addProperty("views", product.getViews());
+            jsonObject.addProperty("company", product.getCompany());
+            JsonArray jsonArray = new JsonArray();
+            for (String s : product.getSpecialPropertiesRelatedToCategory().keySet()) {
+                JsonObject feature = new JsonObject();
+                feature.addProperty("key", s);
+                feature.addProperty("value", product.getSpecialPropertiesRelatedToCategory().get(s));
+                jsonArray.add(feature);
+            }
+            jsonObject.add("specialProperties", jsonArray);
+            allProducts.add(jsonObject);
+        }
+        return allProducts;
     }
 
     public JsonObject addFilter(JsonObject jsonObject, User user) {
