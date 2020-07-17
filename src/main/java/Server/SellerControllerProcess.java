7eb453505edfdafc0de6b41211364713e7e5efb9
hamedrashidpour <45601809+hamedrashidpour@users.noreply.github.com>
2020-07-17 18:28:46 +0430

    SellerControllerProcess add method

--- src/main/java/Server/SellerControllerProcess.java
+++ src/main/java/Server/SellerControllerProcess.java
@@ -2,7 +2,10 @@ package Server;
 
 import Controller.SellerController;
 import Model.User;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+
+import java.util.HashMap;
 
 public class SellerControllerProcess {
     SellerController sellerController = SellerController.getInstance();
@@ -27,5 +30,20 @@ public class SellerControllerProcess {
             return null;
         }
 
+    }
+
+    public JsonObject newProduct(JsonObject jsonObject, User user) {
+        try {
+            HashMap<String, String> specialInformation = new HashMap<>();
+            String[] generalData = new Gson().fromJson(jsonObject.get("generalData").getAsString(), String[].class);
+            String[] first = new Gson().fromJson(jsonObject.get("first").getAsString(), String[].class);
+            String[] second = new Gson().fromJson(jsonObject.get("second").getAsString(), String[].class);
+            for (int i = 0; i < first.length; i++)
+                specialInformation.put(first[i], second[i]);
+            sellerController.addProduct(generalData, user, specialInformation);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jsonObject;
     }
 }
