546c8289bd276a5b6326368d50b22c4a0eb5803a
hamedrashidpour <45601809+hamedrashidpour@users.noreply.github.com>
2020-07-19 01:25:39 +0430

    completing RequestArea

--- src/main/java/GraphicalView/RequestArea.java
+++ src/main/java/GraphicalView/RequestArea.java
@@ -1,7 +1,11 @@
 package GraphicalView;
 
-import Controller.ManagerController;
-import Model.*;
+import Model.OffRequest;
+import Model.SellerOfProductRequest;
+import Model.SellerRequest;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
@@ -11,6 +15,9 @@ import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
@@ -18,20 +25,35 @@ import java.util.stream.Collectors;
 public class RequestArea implements Initializable {
     public VBox detail;
     public Button logout;
-    Request request;
+    String requestId;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         logoutAlert();
-        request = RequestMenu.request;
-        if (request instanceof SellerRequest) {
-            setSellerRequest();
-        } else if (request instanceof OffRequest) {
-            setOffRequest();
-        } else if (request instanceof ProductRequest) {
-            setProductRequest();
+        requestId = RequestMenu.requestToView;
+        dataInputStream = DataBase.getInstance().dataInputStream;
+        dataOutputStream = DataBase.getInstance().dataOutputStream;
+        JsonObject output = Runner.getInstance().jsonMaker("manager", "showRequest");
+        output.addProperty("id", requestId);
+        JsonObject jsonObject = null;
+        try {
+            dataOutputStream.writeUTF(output.toString());
+            dataOutputStream.flush();
+            String input = dataInputStream.readUTF();
+            jsonObject = (JsonObject) new JsonParser().parse(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (jsonObject.get("type").getAsString().equals("seller")) {
+            setSellerRequest(jsonObject);
+        } else if (jsonObject.get("type").getAsString().equals("off")) {
+            setOffRequest(jsonObject);
+        } else if (jsonObject.get("type").getAsString().equals("product")) {
+            setProductRequest(jsonObject);
         } else {
-            setSellerOfProductRequest();
+            setSellerOfProductRequest(jsonObject);
         }
     }
 
@@ -50,98 +72,88 @@ public class RequestArea implements Initializable {
         logout.setOnAction(event);
     }
 
-    private void setSellerRequest() {
-        SellerRequest sellerRequest = (SellerRequest) request;
+    private void setSellerRequest(JsonObject input) {
         Label type = new Label("Request to be a seller");
-        Label username = new Label("username: " + sellerRequest.getUsername());
-        Label name = new Label("name: " + sellerRequest.getInformation()[0] + ", " + sellerRequest.getInformation()[1]);
-        Label company = new Label("company: " + sellerRequest.getInformation()[5]);
-        Label email = new Label("email address: " + sellerRequest.getInformation()[2]);
-        Label phone = new Label("phone number: " + sellerRequest.getInformation()[3]);
+        Label username = new Label("username: " + input.get("username").getAsString());
+        Label name = new Label("name: " + input.get("name").getAsString() + ", " + input.get("lastName").getAsString());
+        Label company = new Label("company: " + input.get("company").getAsString());
+        Label email = new Label("email address: " + input.get("email").getAsString());
+        Label phone = new Label("phone number: " + input.get("phone").getAsString());
         detail.getChildren().addAll(type, username, name, company, email, phone);
     }
 
-    private void setOffRequest() {
-        OffRequest offRequest = (OffRequest) request;
-        if (offRequest.getIsEditing()) {
-            Label type = new Label("Request to edit an off");
-            Label id = new Label("OffId: " + offRequest.getOff().getOffId());
-            Label beginTime = new Label("begin time: " + offRequest.getBeginTime());
-            Label endTime = new Label("end time: " + offRequest.getEndTime());
-            Label percent = new Label("percent: " + offRequest.getOffPercent());
-            Label products = new Label("products are: ");
-            detail.getChildren().addAll(type, id, beginTime, endTime, percent, products);
-            int i = 1;
-            for (Product product : offRequest.getOnSaleProducts()) {
-                Label pro = new Label((i++) + ", " + product.getName());
-                detail.getChildren().add(pro);
-            }
+    private void setOffRequest(JsonObject input) {
+        Label type;
+        if (input.get("isEditing").getAsBoolean()) {
+            type = new Label("Request to edit an off");
         } else {
-            Off off = offRequest.getOff();
-            Label type = new Label("Request to create an off");
-            Label id = new Label("OffId: " + off.getOffId());
-            Label beginTime = new Label("begin time: " + off.getBeginTime());
-            Label endTime = new Label("end time: " + off.getEndTime());
-            Label percent = new Label("percent: " + off.getOffPercent());
-            Label products = new Label("products are: ");
-            detail.getChildren().addAll(type, id, beginTime, endTime, percent, products);
-            int i = 1;
-            for (Product product : off.getOnSaleProducts()) {
-                Label pro = new Label((i++) + ", " + product.getName());
-                detail.getChildren().add(pro);
-            }
+            type = new Label("Request to create an off");
+        }
+        Label id = new Label("OffId: " + input.get("offId").getAsString());
+        Label beginTime = new Label("begin time: " + input.get("beginTime").getAsString());
+        Label endTime = new Label("end time: " + input.get("endTime").getAsString());
+        Label percent = new Label("percent: " + input.get("percent").getAsInt());
+        Label products = new Label("products are: ");
+        detail.getChildren().addAll(type, id, beginTime, endTime, percent, products);
+        int i = 1;
+        for (JsonElement element : input.getAsJsonArray("products")) {
+            Label pro = new Label((i++) + ", " + element.toString());
+            detail.getChildren().add(pro);
         }
     }
 
-    private void setProductRequest() {
-        ProductRequest productRequest = (ProductRequest) request;
-        if (productRequest.isEditing()) {
-            Label type = new Label("Request to edit Product");
-            Label product = new Label("Product: " + productRequest.getProduct().getName());
-            Label seller = new Label("seller: " + productRequest.getSeller().getUsername());
-            Label price = new Label("price: " + productRequest.getPrice());
-            Label available = new Label("available: " + productRequest.getAvailable());
-            Label information = new Label("information: " + productRequest.getInformation());
-            Label features = new Label("special features: ");
-            detail.getChildren().addAll(type, product, seller, price, available, information, features);
-            for (String s : productRequest.getSpecialPropertiesRelatedToCategory().keySet()) {
-                Label f = new Label("key: " + s + ", value: " + productRequest.getSpecialPropertiesRelatedToCategory().get(s));
-                detail.getChildren().add(f);
-            }
+    private void setProductRequest(JsonObject input) {
+        Label type;
+        if (input.get("isEditing").getAsBoolean()) {
+            type = new Label("Request to edit Product");
         } else {
-            Product productToCreate = productRequest.getProduct();
-            Label type = new Label("Request to create Product");
-            Label product = new Label("Product: " + productToCreate.getName());
-            Seller seller = productToCreate.getSellersOfThisProduct().keySet().stream().collect(Collectors.toList()).get(0);
-            Label sellerLabel = new Label("seller: " + seller.getUsername());
-            Label price = new Label("price: " + productToCreate.getMinimumPrice());
-            Label available = new Label("available: " + productToCreate.getAvailable());
-            Label information = new Label("information: " + productToCreate.getInformation());
-            Label features = new Label("special features: ");
-            detail.getChildren().addAll(type, product, sellerLabel, price, available, information, features);
-            for (String s : productToCreate.getSpecialPropertiesRelatedToCategory().keySet()) {
-                Label f = new Label("key: " + s + ", value: " + productToCreate.getSpecialPropertiesRelatedToCategory().get(s));
-                detail.getChildren().add(f);
-            }
+            type = new Label("Request to create Product");
+        }
+        Label product = new Label("Product: " + input.get("name").getAsString());
+        Label seller = new Label("seller: " + input.get("seller").getAsString());
+        Label price = new Label("price: " + input.get("price").getAsInt());
+        Label available = new Label("available: " + input.get("available").getAsInt());
+        Label information = new Label("information: " + input.get("information").getAsString());
+        Label features = new Label("special features: ");
+        detail.getChildren().addAll(type, product, seller, price, available, information, features);
+        for (JsonElement element : input.getAsJsonArray("special")) {
+            JsonObject feature = element.getAsJsonObject();
+            Label f = new Label("key: " + feature.get("key").getAsString() + ", value: " + feature.get("value").getAsString());
+            detail.getChildren().add(f);
         }
     }
 
-    private void setSellerOfProductRequest() {
-        SellerOfProductRequest sellerOfProductRequest = (SellerOfProductRequest) request;
+    private void setSellerOfProductRequest(JsonObject input) {
         Label type = new Label("Request to add a seller to a product");
-        Label product = new Label("Product: " + sellerOfProductRequest.getProduct());
-        Label seller = new Label("seller: " + sellerOfProductRequest.getSeller());
-        Label price = new Label("price: " + sellerOfProductRequest.getPrice());
+        Label product = new Label("ProductId: " + input.get("product").getAsString());
+        Label seller = new Label("seller: " + input.get("seller").getAsString());
+        Label price = new Label("price: " + input.get("price").getAsInt());
         detail.getChildren().addAll(type, product, seller, price);
     }
 
     public void accept(MouseEvent mouseEvent) {
-        ManagerController.getInstance().acceptRequest(request.getRequestId());
+        JsonObject jsonObject = Runner.getInstance().jsonMaker("manager", "acceptRequest");
+        jsonObject.addProperty("id", requestId);
+        try {
+            dataOutputStream.writeUTF(jsonObject.toString());
+            dataOutputStream.flush();
+            dataInputStream.readUTF();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         back(mouseEvent);
     }
 
     public void decline(MouseEvent mouseEvent) {
-        ManagerController.getInstance().declineRequest(request.getRequestId());
+        JsonObject jsonObject = Runner.getInstance().jsonMaker("manager", "declineRequest");
+        jsonObject.addProperty("id", requestId);
+        try {
+            dataOutputStream.writeUTF(jsonObject.toString());
+            dataOutputStream.flush();
+            dataInputStream.readUTF();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         back(mouseEvent);
     }
 
