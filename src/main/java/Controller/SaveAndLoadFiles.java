1dcb1052a24b46953787c903cd3e6345d31af439
ASUS <mohammad.cheraghi80@gmail.com>
2020-07-24 22:19:43 +0430

    debug

--- src/main/java/Controller/SaveAndLoadFiles.java
+++ src/main/java/Controller/SaveAndLoadFiles.java
@@ -2,6 +2,10 @@ package Controller;
 
 import Model.Auction;
 import Model.*;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.*;
 
 public class SaveAndLoadFiles {
     public static void start() {
@@ -16,6 +20,16 @@ public class SaveAndLoadFiles {
         Request.fileToLog();
         Supporter.fileToLog();
         Auction.fileToLog();
+        try {
+            FileInputStream file = new FileInputStream("src/project files/information.txt");
+            DataInputStream in = new DataInputStream(file);
+            String input = in.readUTF();
+            JsonObject jsonObject = (JsonObject) new JsonParser().parse(input);
+            Manager.minInventory = jsonObject.get("min").getAsInt();
+            Manager.wagePercent = jsonObject.get("percent").getAsInt();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void end() {
@@ -30,5 +44,16 @@ public class SaveAndLoadFiles {
         Request.logToFile();
         Supporter.logToFile();
         Auction.logToFile();
+        try {
+            FileOutputStream file = new FileOutputStream("src/project files/information.txt");
+            DataOutputStream out = new DataOutputStream(file);
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("min", Manager.minInventory);
+            jsonObject.addProperty("percent", Manager.wagePercent);
+            out.writeUTF(jsonObject.toString());
+            out.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
